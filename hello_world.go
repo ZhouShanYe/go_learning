3d5d50f50534867e6486a69c491ce2de93d251de
zhoushanye <915507430@qq.com>
2022-01-25 22:12:08 +0800

    modify user_server

--- hello_world.go
+++ hello_world.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
-func mian(){
-	fmt.Println("hello world")
+type Person struct{
+	name string
+	age int
+	male bool
 }
+
+func (p Person) gao() {
+	fmt.Println("gao")
+}
+func main(){
+	fmt.Println("hello world")
+	a := 0
+	fmt.Println(reflect.TypeOf(a), reflect.TypeOf(a).Kind())
+
+	b := &a
+	fmt.Println(reflect.TypeOf(b), reflect.TypeOf(b).Kind(), reflect.TypeOf(b).Elem())
+
+	p := Person{}
+	fmt.Println(reflect.TypeOf(p), reflect.TypeOf(p).Kind())
+	typeOfP := reflect.TypeOf(p)
+	for i:=0; i< typeOfP.NumField(); i++ {
+		fmt.Println('1', typeOfP.Field(i).Name, typeOfP.Field(i).Type, typeOfP.Field(i).Type.Kind())
+	}
+	for i:=0; i< typeOfP.NumMethod(); i++ {
+		fmt.Println('2', typeOfP.Method(i).Name, typeOfP.Method(i).Type, typeOfP.Method(i).Type.Kind())
+	}
+}
+
--- user_server/main.go
+++ user_server/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
-	"encoding/json"
 	"log"
 	"net/http"
 	"strings"
 )
 
 type User struct {
-	Name   string
-	Habits []string
+	Id       int
+	Name     string
+	Password string
+	Habits   []string
 }
+
+var UserById = make(map[int]*User)
+var UserByName = make(map[string][]*User)
 
 func sayHelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
@@ -36,8 +41,11 @@ func login(w http.ResponseWriter, r *http.Request) {
 		log.Println(t.Execute(w, nil))
 	} else {
 		_ = r.ParseForm()
-		fmt.Println("username:", r.Form["usename"])
+		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
+
+		user := User{1, r.Form.Get("username"), r.Form.Get("password"), []string{"学习"}}
+		store(user)
 		if pwd := r.Form.Get("password"); pwd == "123456" {
 			fmt.Fprintf(w, "欢迎登陆， %s!", r.Form.Get("username"))
 		} else {
@@ -46,14 +54,27 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func store(user User) {
+	UserById[user.Id] = &user
+	UserByName[user.Name] = append(UserByName[user.Name], &user)
+}
+
+func loginInfo(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	for _, user := range UserByName[r.Form.Get("username")] {
+		fmt.Fprintf(w, "%v", user)
+	}
+}
 func write(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("X-custom-Header", "custom")
 	w.WriteHeader(201)
 
 	user := &User{
-		Name:   "zhoushanye",
-		Habits: []string{"吃饭", "睡觉"},
+		Id:       0,
+		Name:     "zhoushanye",
+		Password: "123456",
+		Habits:   []string{"吃饭", "睡觉"},
 	}
 	json, _ := json.Marshal(user)
 	w.Write(json)
@@ -63,6 +84,7 @@ func main() {
 
 	http.HandleFunc("/", sayHelloName)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/login_info", loginInfo)
 	http.HandleFunc("/write", write)
 	err := http.ListenAndServe("0.0.0.0:9090", nil)
 	if err != nil {
